e669fdb7f34e1b9e07c91ab5405b3db4234a4f9b
aliciagarciapires <aliciagarciapiress@gmail.com>
2026-09-16 17:44:14 -0300

    mudei pro lince

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,3 +1,3 @@
 export const API_URL =
-  "http://10.0.0.104/DiarioInclusivo/src/app";
-  //"https://diarioinclusivo.linceonline.com.br";
+  //"http://10.0.0.104/DiarioInclusivo/src/app";
+  "https://diarioinclusivo.linceonline.com.br";

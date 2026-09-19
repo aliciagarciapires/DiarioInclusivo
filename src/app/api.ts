07d2e08c0eb98a7cee657f99efce4894a214852e
aliciagarciapires <aliciagarciapiress@gmail.com>
2026-09-19 10:35:47 -0300

    mudei o fetch so

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,3 +1,3 @@
 export const API_URL =
-  "http://192.168.0.106/DiarioInclusivo/src/app";
-  //"https://diarioinclusivo.linceonline.com.br";
+  //"http://192.168.0.106/DiarioInclusivo/src/app";
+  "https://diarioinclusivo.linceonline.com.br";
